81e60bd4781751bdfb1d87d9430ca707dd919c64
allan <info@allangrunert.de>
2025-04-25 14:56:50 +0200

    example

---
+++ app/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace csharp_coingecko_selenium_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--log-level=3"); // Suppress logs
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.coingecko.com/en/coins/bitcoin");
+            var title = driver.Title;
+            Console.WriteLine("Page title is: " + title);            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(50);
+            var bitcoinPrice = driver.FindElements(By.XPath("//span[@data-price-btc='1.0']"));
+
+            foreach (var price in bitcoinPrice)
+            {
+                if (price.GetAttribute("data-coin-id") != null && price.GetAttribute("data-coin-id") == "1" && price.Text != "")
+                {
+                    Console.WriteLine("Current Bitcoin price on Coingecko is: " + price.Text);
+                    break;
+                }
+            }
+            
+            driver.Quit();
+        }
+    }
+}
---
+++ testApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// dotnet run --project TestApp
+// See https://aka.ms/new-console-template for more information
+// var str = string.Format("{0:n}", 123456);
+var str = string.Format("{0:c}", 123456);
+Console.WriteLine(String.Format("Hello, World! {0}", str));
+try 
+{
+throw(new System.NullReferenceException()); 
+} catch(Exception e)
+{
+    Console.WriteLine( as String);
+}
